77f2d9c7f0a53cc1313bb18e488cd5b3b1685762
Daniel Gwilt <dgwilt@fffenterprises.com>
2024-10-27 13:56:52 -0700

    Reduce spy and embargo range to 5

--- src/ungp/impl/rules/economy/UNGP_TradeEmbargo.java
+++ src/ungp/impl/rules/economy/UNGP_TradeEmbargo.java
@@ -9,43 +9,44 @@ import ungp.api.rules.tags.UNGP_EconomyTag;
 import java.util.List;
 
 public class UNGP_TradeEmbargo extends UNGP_BaseRuleEffect implements UNGP_EconomyTag {
-    @Override
-    public float getValueByDifficulty(int index, UNGP_SpecialistSettings.Difficulty difficulty) {
-        return 0;
-    }
+	@Override
+	public float getValueByDifficulty(int index, UNGP_SpecialistSettings.Difficulty difficulty) {
+		return 0;
+	}
 
+	@Override
+	public String getDescriptionParams(int index, UNGP_SpecialistSettings.Difficulty difficulty) {
+		if (index == 0)
+			return "5";
+		if (index == 1)
+			return "10%";
+		return super.getDescriptionParams(index, difficulty);
+	}
 
-    @Override
-    public String getDescriptionParams(int index, UNGP_SpecialistSettings.Difficulty difficulty) {
-        if (index == 0) return "15";
-        if (index == 1) return "10%";
-        return super.getDescriptionParams(index, difficulty);
-    }
+	@Override
+	public void applyPlayerMarket(MarketAPI market) {
+		List<MarketAPI> nearbyMarkets = Misc.getNearbyMarkets(market.getLocationInHyperspace(), 5);
+		int hostileCount = 0;
+		for (MarketAPI nearbyMarket : nearbyMarkets) {
+			if (nearbyMarket.getFaction().isHostileTo(market.getFaction())) {
+				hostileCount++;
+			}
+		}
+		market.getAccessibilityMod().modifyFlat(buffID, -hostileCount * 0.1f, rule.getName());
+	}
 
-    @Override
-    public void applyPlayerMarket(MarketAPI market) {
-        List<MarketAPI> nearbyMarkets = Misc.getNearbyMarkets(market.getLocationInHyperspace(), 15);
-        int hostileCount = 0;
-        for (MarketAPI nearbyMarket : nearbyMarkets) {
-            if (nearbyMarket.getFaction().isHostileTo(market.getFaction())) {
-                hostileCount++;
-            }
-        }
-        market.getAccessibilityMod().modifyFlat(buffID, -hostileCount * 0.1f, rule.getName());
-    }
+	@Override
+	public void unapplyPlayerMarket(MarketAPI market) {
+		market.getAccessibilityMod().unmodify(buffID);
+	}
 
-    @Override
-    public void unapplyPlayerMarket(MarketAPI market) {
-        market.getAccessibilityMod().unmodify(buffID);
-    }
+	@Override
+	public void applyAllMarket(MarketAPI market) {
 
-    @Override
-    public void applyAllMarket(MarketAPI market) {
+	}
 
-    }
+	@Override
+	public void unapplyAllMarket(MarketAPI market) {
 
-    @Override
-    public void unapplyAllMarket(MarketAPI market) {
-
-    }
+	}
 }
